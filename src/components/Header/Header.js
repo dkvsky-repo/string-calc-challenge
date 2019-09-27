2cb240f7fa2b5075fd0331e6d2fee00867ec843b
David Kontorovsky <programatiko@pm.me>
2019-09-27 10:59:01 -0700

    R365 challenge step 1.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,10 @@ const Header = () => {
   return (
     <header className='jumbotron jumbotron-fluid'>
       <div className='container'>
-        <h1 className='display-4'>
-          Code Challenge: <br />
-          String Calculator
-        </h1>
+        <h1 className='display-4'>Code Challenge: String Calculator</h1>
         <p className='lead'>
-          A calculator that only supports an Add operation <br />
-          given a single formatted string.
+          A calculator that only supports an Add operation given a single
+          formatted string.
         </p>
       </div>
     </header>
---
+++ src/utils/utils.js
@@ -0,0 +1,33 @@
+/**
+ * Extract operands from string value.
+ *
+ * @param {String} stringValue - String value from form.
+ * @param {String} delimiter - Delimiter for splitting operation.
+ * @param {Number} itemLimit - Number of results expected; i.e. 2 in step 1.
+ *
+ * @returns {Array} An array of numbers.
+ */
+export function getOperands(stringValue, { delimiter, itemLimit } = {}) {
+  // Step 1.
+  // - Support 2 numbers using comma delimiter
+  // - Convert invalid/missing numbers to zero
+  const operands = stringValue.split(delimiter).map(operand => {
+    operand = operand.trim();
+    if (isNaN(operand) || operand === '') {
+      operand = 0;
+    }
+    return parseInt(operand);
+  });
+  return itemLimit ? operands.slice(0, itemLimit) : operands;
+}
+
+/**
+ * Perform sum on operands array.
+ *
+ * @param {Array} operandArray - Array of number operands.
+ *
+ * @returns {Number} A number value.
+ */
+export function sumEntries(operandArray) {
+  return operandArray.reduce((acc, cur) => acc + cur, 0);
+}
